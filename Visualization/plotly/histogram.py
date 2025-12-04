3110bc509da767807172dc036c40c1a79717e3d6
Varshini Thota <varshinipatel001@gmail.com>
2025-12-04 17:53:11 +0530

    Refactor histogram code for improved readability

--- Visualization/plotly/histogram.py
+++ Visualization/plotly/histogram.py
@@ -3,10 +3,6 @@ import plotly.express as px
 
 file_path="../../original_files/updated_dataset.csv"
 df=pd.read_csv(file_path)
-fig = px.histogram(
-    df,
-    x="Units_Sold",
-    nbins=10,
-    title=f"Distribution of Units Sold "
-)
+fig = px.histogram(df,x="Units_Sold",nbins=10, title=f"Distribution of Units Sold ")
 fig.show()
+
