6789a6f8c8b43cb268959993b056f051afcd418c
Varshini Thota <varshinipatel001@gmail.com>
2025-12-04 17:53:52 +0530

    Refactor pie chart code for conciseness

--- Visualization/plotly/pie_chart.py
+++ Visualization/plotly/pie_chart.py
@@ -3,11 +3,7 @@ import plotly.express as px
 
 file_path="../../original_files/updated_dataset.csv"
 df=pd.read_csv(file_path)
-fig = px.pie(
-    df,
-    names="Product_Name",
-    values="Total_Sales_Value",
-    title="Sales Contribution by Product"
-)
+fig = px.pie(df,names="Product_Name",values="Total_Sales_Value",title="Sales Contribution by Product")
 fig.show()
+
 
